3e31f5dd4edb922d18be912d664f17188344db85
Rômulo <romulojnr@gmail.com>
2023-11-24 14:24:19 -0300

    Prova 2

--- main_test.py
+++ main_test.py
@@ -1,62 +1,50 @@
-import mock
-import builtins
-import main
+# Testes parametrizados corrigidos
+import pytest
+from solution import q1, q2, q3, q4
 
 
-def test_q1(capfd):
-    input_output = {
-        '7': 'O número não possui divisores multiplos de 3\n',
-        '3': 'Quantidade de divisores divisiveis por 3: 1\n',
-        '555':'Quantidade de divisores divisiveis por 3: 4\n',
-        '3144':'Quantidade de divisores divisiveis por 3: 8\n',
-        '17640':'Quantidade de divisores divisiveis por 3: 48\n'
-    }
-    for k, v in input_output.items():
-        with mock.patch.object(builtins, 'input', lambda _: k):
-            main.q1()
-            out, err = capfd.readouterr()
-            assert out == v
+@pytest.mark.parametrize("entrada, esperado", [
+    ({}, []),
+    ({"Joao": 25, "Maria": 20, "Ana": 30}, ["Ana", "Joao", "Maria"]),
+    ({"Alice": 17, "Bob": 17, "Charlie": 22}, ["Charlie"]),
+    ({"Lucas": 16, "Sophia": 19, "Pedro": 21}, ["Pedro", "Sophia"]),
+])
+def test_q1(entrada, esperado):
+    resultado = q1(entrada)
+    assert resultado == esperado
 
-def test_q2(capfd, monkeypatch):
-    input_output = {
-        '1,3': '6\n',
-        '-2,2': '3\n',
-        '10,-10': '55\n',
-    }
-    for k,v in input_output.items():
-        monkeypatch.setitem(__builtins__, 'input', make_multiple_inputs(k.split(',')))
-        main.q2()
-        out, _ = capfd.readouterr()
-        assert out == v
+# Casos de teste parametrizados
+@pytest.mark.parametrize("lista1, lista2, esperado", [
+    ([1, 3, 5], [2, 4, 6, 8, 10], [1, 2, 3, 4, 5, 6, 8, 10]),
+    ([], [2, 4, 6], [2, 4, 6]),
+    ([1, 3, 5, 7], [2, 4], [1, 2, 3, 4, 5, 7]),
+    ([1, 3], [2, 4, 6], [1, 2, 3, 4, 6]),
+])
+def test_q2(lista1, lista2, esperado):
+    resultado = q2(lista1, lista2)
+    assert resultado == esperado
+
+@pytest.mark.parametrize("entrada, esperado_pares, esperado_impares", [
+    ([2, 4, 6, 8, 10, 12, 14, 0], [12, 14, 6, 8, 10], []),
+    ([1, 3, 5, 7, 9, 11, 0], [], [11, 3, 5, 7, 9]),
+    ([2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 0], [22, 14, 16, 18, 20], []),
+    ([2, 4, 6, 8, 10, 12, 1, 3, 5, 7, 9, 11, 0], [12, 4, 6, 8, 10], [11, 3, 5, 7, 9])
+])
+def test_processa_lista(entrada, esperado_pares, esperado_impares):
+    resultado_pares, resultado_impares = q3(entrada)
+    assert resultado_pares == esperado_pares
+    assert resultado_impares == esperado_impares
+
+@pytest.mark.parametrize("entrada, esperado", [
+    ([1.75, 1.80, 1.70, 1.65, 0], ['1.65', '1.75', '1.80', '1.70']),
+    ([1.60, 1.62, 1.58, 1.65, 0], ['1.58', '1.62', '1.65', '1.60']),
+    ([1.70, 1.68, 1.75, 1.60, 0], ['1.60', '1.70', '1.75', '1.68']),
+    ([1.80, 1.75, 1.90, 1.88, 0], ['1.75', '1.88', '1.90', '1.80']),
+    ([1.65, 1.60, 1.58, 1.70, 0], ['1.58', '1.65', '1.70', '1.60']),
+    # Adicione mais casos conforme necessário
+])
+def test_q2(entrada, esperado):
+    resultado = q4(entrada)
+    assert resultado == esperado
 
 
-def make_multiple_inputs(inputs):
-    """ provides a function to call for every input requested. """
-    def next_input(_):
-        """ provides the first item in the list. """
-        return inputs.pop()
-    return next_input
-
-def test_q3(capfd, monkeypatch):
-    input_output = {
-        '1000,500,10,5': 'O custo total mensal estimado para a infraestrutura é de R$ 3600.00.\n',
-        '10000,1000,50,10': 'O custo total mensal está acima do limite.\n',
-    }
-    for k,v in input_output.items():
-        monkeypatch.setitem(__builtins__, 'input', make_multiple_inputs(k.split(',')))
-        main.q3()
-        out, _ = capfd.readouterr()
-        assert out == v
-
-
-def test_q4(capfd):
-    input_output = {
-        '0': '0.0\n',
-        '8': '1/3 + 2/6 + 3/9 + 4/12 + 5/15 + 6/18 + 7/21 + 8/24\n2.67\n',
-        '4': '1/3 + 2/6 + 3/9 + 4/12\n1.33\n',
-    }
-    for k, v in input_output.items():
-        with mock.patch.object(builtins, 'input', lambda _: k):
-            main.q4()
-            out, err = capfd.readouterr()
-            assert out == v
